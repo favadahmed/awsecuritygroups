2220979d891d57654c56b0cb8f51ccc5dae2de2f
Ubuntu <ubuntu@ip-10-86-1-35.eu-west-2.compute.internal>
2016-12-21 10:46:08 +0000

    first commit

---
+++ sginfo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import boto.ec2
+import hashlib
+
+try:  
+  conn = boto.ec2.connect_to_region("eu-west-2")
+
+  current_sgs = conn.get_all_security_groups()
+except boto.exception.BotoServerError,e:log.error(e.error_message)
+conn.close()
+
+for sg in current_sgs:
+  print "="*80
+  print "id:\t\t", sg.id
+  print "name:\t\t", sg.name
+  print "vpc:\t\t", sg.vpc_id
+  print "instance:\t", sg.instances()
+  for rule in sg.rules:
+    ruledata = sg.id,rule.grants,rule.ip_protocol,rule.from_port,rule.to_port,"ingress"
+    rulehash=hashlib.sha256(str(ruledata)).hexdigest()
+    print"\thash:",rulehash
+    print "\t",rule.grants,"-> [instance]:",rule.from_port,"-",rule.to_port,rule.ip_protocol
+    print "="*72
+
+for rule in sg.rules_egress:
+    ruletuple = sg.id,rule.grants,rule.ip_protocol,rule.from_port,rule.to_port,"ingress"
+    rulehash=hashlib.sha256(str(ruletuple)).hexdigest()
+    print"\thash:",rulehash
+    print "\t[instance]->",rule.grants,":",rule.from_port,"-",rule.to_port,rule.ip_protocol
+
